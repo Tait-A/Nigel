4455f49e10f5a5050df0d035bab43313a5073eb7
Tait-A <s2051131@ed.ac.uk>
2024-03-20 15:48:51 +0000

    control

--- src/Motoring/car_control.py
+++ src/Motoring/car_control.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from motor_utils import CarControl
+import sys
+import os
 
-import RPi.GPIO as GPIO 
-from time import sleep
-from adafruit_servokit import ServoKit
-
-
-kit = ServoKit(channels=16)
-
-GPIO.setmode(GPIO.BCM)
-#Pins 18 22 24 GPIO 24 25 8
-Motor1E = 18 #  Enable pin 1 of the controller IC
-Motor1A = 24 #  Input 1 of the controller IC
-Motor1B = 23 #  Input 2 of the controller IC
+# add ../ to path
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from Models.robot import Robot
+from Utils.action import Action
+from Utils.trajectory import Trajectory
 
 
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1E,GPIO.OUT)
+if __name__ == "__main__":
+    car = Robot()
+    car_control = CarControl(car)
 
-reverse=GPIO.PWM(Motor1A,100) # configuring Enable pin for PWM
-forward=GPIO.PWM(Motor1B,100) # configuring Enable pin for PWM
+    trajectory_file = os.path.join(os.path.dirname(__file__), "../trajectory.json")
 
+    trajectory = Trajectory.from_json(trajectory_file)
 
-
+    for action in trajectory.actions:
+        car_control.process_action(action)
